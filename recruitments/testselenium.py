944c3b829ab5df6c9545d6019f44e88b85f09965
Bartłomiej Dębowski <bartek.993@gmail.com>
2022-09-04 16:25:17 +0200

    test works version 2

--- recruitments/testselenium.py
+++ recruitments/testselenium.py
@@ -6,6 +6,25 @@ import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(service=Service(PATH))
+ebook_partial_name = "online_consumer"
+data = {
+    "name": "Bartłomiej Debowski",
+    "email": "bartlomiej.debowski.benhauer+testrekrutacja@salesmanago.com",
+    "company": "SALESmanago",
+    "url": "www.salesmanago.pl",
+    "phoneNumber": "123456789",
+}
+
+
+def fill_the_blanks(form_list, info):
+    for k, v in info.items():
+        for i in form_list:
+            if i.get_attribute("name") == k:
+                i.send_keys(v)
+            elif i.get_attribute("id") == k:
+                i.send_keys(v)
+    return fill_the_blanks
+
 
 driver.get("https://www.salesmanago.com/")
 sources = driver.find_elements(by=By.CLASS_NAME, value="menu__link.menu__item--show")
@@ -20,24 +39,14 @@ driver.get(ebooks)
 get_ebook = driver.find_elements(by=By.CLASS_NAME, value="col-lg-3.ebook__text")
 for ebook in get_ebook:
     one = ebook.find_element(by=By.TAG_NAME, value="a").get_attribute("href")
-    if "online_consumer" in one:
+    if ebook_partial_name in one:
         form = one
         break
 
 driver.get(form)
 
 edities = driver.find_elements(by=By.CLASS_NAME, value="form-control")
-for i in edities:
-    if i.get_attribute("name") == "name":
-        i.send_keys("Bartłomiej Debowski")
-    elif i.get_attribute("id") == "email":
-        i.send_keys("bartlomiej.debowski@salesmanago.com")
-    elif i.get_attribute("name") == "company":
-        i.send_keys("SALESmanago")
-    elif i.get_attribute("name") == "url":
-        i.send_keys("www.salesmanago.pl")
-    elif i.get_attribute("name") == "phoneNumber":
-        i.send_keys("123456789")
+fill_the_blanks(edities, data)
 
 submit_button = driver.find_element(by=By.CLASS_NAME, value="btn.center-block.form-btn")
 submit_button.click()
