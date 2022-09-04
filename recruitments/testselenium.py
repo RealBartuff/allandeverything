1b0898ecd066836138a6f99c9b5cad0d0cc1433b
Bartłomiej Dębowski <bartek.993@gmail.com>
2022-09-04 16:51:44 +0200

    Website, ebook name and user data attached to variables
    Add function for fill in the user form
    
    Add message about successfully downloaded ebook

--- recruitments/testselenium.py
+++ recruitments/testselenium.py
@@ -6,8 +6,9 @@ import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(service=Service(PATH))
-ebook_partial_name = "online_consumer"
-data = {
+website = "https://www.salesmanago.com/"
+ebook_name = "online_consumer_trends"      # change HERE to get other ebook
+data_form = {
     "name": "Bartłomiej Debowski",
     "email": "bartlomiej.debowski.benhauer+testrekrutacja@salesmanago.com",
     "company": "SALESmanago",
@@ -16,8 +17,8 @@ data = {
 }
 
 
-def fill_the_blanks(form_list, info):
-    for k, v in info.items():
+def fill_the_blanks(form_list, data):
+    for k, v in data.items():
         for i in form_list:
             if i.get_attribute("name") == k:
                 i.send_keys(v)
@@ -26,7 +27,7 @@ def fill_the_blanks(form_list, info):
     return fill_the_blanks
 
 
-driver.get("https://www.salesmanago.com/")
+driver.get(website)
 sources = driver.find_elements(by=By.CLASS_NAME, value="menu__link.menu__item--show")
 for elem in sources:
     if elem.get_attribute("data-number") == "3":
@@ -39,18 +40,18 @@ driver.get(ebooks)
 get_ebook = driver.find_elements(by=By.CLASS_NAME, value="col-lg-3.ebook__text")
 for ebook in get_ebook:
     one = ebook.find_element(by=By.TAG_NAME, value="a").get_attribute("href")
-    if ebook_partial_name in one:
+    if ebook_name in one and len(ebook_name) >= 10:
         form = one
         break
 
 driver.get(form)
 
 edities = driver.find_elements(by=By.CLASS_NAME, value="form-control")
-fill_the_blanks(edities, data)
+fill_the_blanks(edities, data_form)
 
 submit_button = driver.find_element(by=By.CLASS_NAME, value="btn.center-block.form-btn")
 submit_button.click()
 
 time.sleep(3)
-
+print("Ebook {} downloaded!".format(ebook_name))
 driver.quit()
